4cd025362c66d5fdecf4f0ec0ef2c0d83f844924
Michel Vocks <michel.vocks@dbschenker.com>
2018-12-04 10:39:06 +0100

    Added vault example

--- main.cc
+++ main.cc
@@ -15,26 +15,15 @@ void PrintParam(list<argument> args) throw(string) {
 }
 
 int main() {
-    std::list<job> jobs;
+    list<job> jobs;
     job printparam;
     printparam.handler = &PrintParam;
     printparam.title = "Print Parameters";
-    printparam.description = "This job prints out all given params.";
+    printparam.description = "This job prints out all given params which includes one from the vault.";
     printparam.args.push_back(argument{
-        "Username for the database schema:",
-        // textfield displays a text field in the UI.
-        // You can also use textarea for text area, boolean
-        // for boolean input.
-        gaia::InputType::input_type::textfield,
-        "username",
-    });
-    printparam.args.push_back(argument{
-        "Description for username:",
-        // textfield displays a text field in the UI.
-        // You can also use textarea for text area, boolean
-        // for boolean input.
-        gaia::InputType::input_type::textarea,
-        "usernamedesc",
+        "",
+        gaia::InputType::input_type::vault,
+        "dbpassword",
     });
 
     jobs.push_back(printparam);
